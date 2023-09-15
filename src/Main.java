80bb415110180c36792aa3800717cb4efb1ea5c2
atime <2860421534@qq.com>
2023-09-15 14:07:58 +0800

    test6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! 77788901");
+        System.out.println("Hello world! 77788901000");
     }
 }
